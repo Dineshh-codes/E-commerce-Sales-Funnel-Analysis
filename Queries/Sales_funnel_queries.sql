798d1e63f6a72ca1efde34cf03c8b4f2313684d1
Dineshkumar S <dineshhkumar1998@gmail.com>
2026-03-06 15:00:58 +0530

    Add files via upload

---
+++ Queries/Sales_funnel_queries.sql
@@ -0,0 +1,96 @@
+SELECT * FROM `sales-funnel-analysis-488715.sales_funnel.user_events` LIMIT 1000;
+
+--- conversion rate through the funnel ---- 
+
+WITH funnel_stages AS (
+    SELECT
+      COUNT(DISTINCT CASE WHEN event_type = 'page_view' then user_id end) AS stage_1_views,
+      COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' then user_id end) AS stage_2_cart,
+      COUNT(DISTINCT CASE WHEN event_type = 'checkout_start' then user_id end) AS stage_3_checkout,
+      COUNT(DISTINCT CASE WHEN event_type = 'payment_info' then user_id end) AS stage_4_payment,
+      COUNT(DISTINCT CASE WHEN event_type = 'purchase' then user_id end) AS stage_5_purchase
+    FROM `sales-funnel-analysis-488715.sales_funnel.user_events`
+    WHERE event_date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 47 DAY))
+)
+SELECT 
+     stage_1_views,
+     stage_2_cart,
+     ROUND(stage_2_cart*100/stage_1_views) AS view_to_cart_rate,
+     stage_3_checkout,
+     ROUND(stage_3_checkout*100/stage_2_cart) AS cart_to_checkout_rate,
+     stage_4_payment,
+     ROUND(stage_4_payment*100/stage_3_checkout) AS checkout_to_payment_rate,
+     stage_5_purchase,
+     ROUND(stage_5_purchase*100/stage_4_payment) AS payment_to_purchase_rate,
+     ROUND(stage_5_purchase*100/stage_1_views) AS Overall_rate
+FROM funnel_stages;
+
+
+
+--- funnel by Source --- 
+WITH source_funnel AS (
+    SELECT
+      traffic_source, 
+      COUNT(DISTINCT CASE WHEN event_type = 'page_view' then user_id end) AS views,
+      COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' then user_id end) AS cart,
+      COUNT(DISTINCT CASE WHEN event_type = 'checkout_start' then user_id end) AS checkout,
+      COUNT(DISTINCT CASE WHEN event_type = 'payment_info' then user_id end) AS payment,
+      COUNT(DISTINCT CASE WHEN event_type = 'purchase' then user_id end) AS purchase
+    FROM
+    `sales-funnel-analysis-488715.sales_funnel.user_events`
+    WHERE event_date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 49 DAY))
+    GROUP BY 
+    traffic_source
+)
+SELECT 
+  traffic_source,
+  views,
+  cart,
+  purchase,
+  ROUND(cart * 100 / views) as cart_conversion_rate,
+  ROUND(purchase * 100/ views) as purchase_conversion_rate,
+  ROUND(purchase * 100/ cart) as cart_to_purchase_conversion_rate  
+FROM source_funnel
+ORDER BY purchase DESC;
+
+
+--- time to conversion rate --- 
+WITH user_journey AS (
+  SELECT
+      user_id,
+      MIN(CASE WHEN event_type = 'page_view' then event_date end) AS view_time,
+      MIN(CASE WHEN event_type = 'add_to_cart' then event_date end) AS cart_time,
+      MIN(CASE WHEN event_type = 'purchase' then event_date end) AS purchase_time
+  FROM `sales-funnel-analysis-488715.sales_funnel.user_events`  
+  WHERE event_date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 49 DAY))
+  GROUP BY
+     user_id
+  HAVING MIN(CASE WHEN event_type = 'purchase' THEN event_date END) IS NOT NULL
+  )
+SELECT 
+  COUNT(user_id) AS Converted_users,
+  ROUND(AVG(TIMESTAMP_DIFF(cart_time, view_time, MINUTE)),2) AS avg_view_to_cart_minutes,
+  ROUND(AVG(TIMESTAMP_DIFF(purchase_time,cart_time, MINUTE)),2) AS avg_cart_to_purchase_time,
+  ROUND(AVG(TIMESTAMP_DIFF(purchase_time,view_time, MINUTE)),2) AS avg_total_journey_time,
+FROM user_journey;
+
+
+
+--- Revenue Funnel --- 
+
+WITH funnel_revenue AS (
+  SELECT
+      COUNT(DISTINCT CASE WHEN event_type = 'page_view' then user_id end) AS total_visitors,
+      COUNT(DISTINCT CASE WHEN event_type = 'purchase' then user_id end) AS total_buyers,
+      SUM(CASE WHEN event_type = "purchase" THEN amount END) AS total_revenue,
+      COUNT(CASE WHEN event_type = 'purchase' then user_id end) AS total_orders
+      FROM `sales-funnel-analysis-488715.sales_funnel.user_events`  
+      WHERE event_date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 49 DAY))        
+)
+SELECT 
+  total_visitors,
+  total_buyers,
+  ROUND(total_revenue,2) as total_revenue,
+  ROUND(total_revenue / total_buyers,2) AS average_order_value,
+  ROUND(total_revenue / total_visitors,2) AS average_revenue_per_customer
+FROM funnel_revenue;
